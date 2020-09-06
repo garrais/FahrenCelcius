b86bc2132be93ddefdb4b17bbf9de6a0433e44e0
Gabriel Degarrais dos santos <70085404+garrais@users.noreply.github.com>
2020-09-06 20:45:38 -0300

    Add files via upload

---
+++ CalculoCelcius&Farenheit.py
@@ -0,0 +1,9 @@
+nome = input('Qual seu nome: ')
+celcius = float(input('Digite uma temperatura: '))
+faren = float(celcius*9/5+32)
+print('Olá {}'.format(nome))
+print('valor é: {}'.format(celcius*9/5+32))
+
+
+
+
